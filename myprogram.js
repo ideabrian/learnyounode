391532f0bf5cb81a61f4ff7073836fe1fe8d2c55
Brian Ball <ideabrian@gmail.com>
2014-12-18 01:09:00 -0800

    basic setup

--- myprogram.js
+++ myprogram.js
@@ -1,3 +1,15 @@
 // Learn You Node
 
-// See each of the branches for the code for each step.
+var http = require('http');
+var bl = require('bl');
+var streams = [];
+var urls = [process.argv[2], process.argv[3], process.argv[4] ];
+
+input = process.argv[2];
+
+http.get(input, function(response){
+	response.setEncoding('utf8');
+	console.log("Got response: " + response.statusCode);
+}).on('error', function(e) {
+		console.log("Get error" + e.message);
+	});
