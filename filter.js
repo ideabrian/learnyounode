8f90fb386558a2e6610a2412a9cbf749f228b23f
Brian Ball <ideabrian@gmail.com>
2014-12-17 22:31:38 -0800

    FILTERED LS

---
+++ filter.js
@@ -0,0 +1,22 @@
+
+// we need to import the fs module so we can use the readdir() function
+var fs = require('fs');
+// require the path module so we can use the path.extname(file) to verify the filename
+var path = require('path');
+
+// export the function so we can use it from our main file
+// this is what's known as an API
+
+// this API takes a path, an extension, and a callback argument
+// 
+module.exports = function (input, ext, callback) {
+	fs.readdir(input, function (err, files){
+		if(err)
+		  return callback(err) // early return
+		// console.log(files);
+		filtered = files.filter(function (e) {
+		  return path.extname(e) === '.'+ ext;
+		});
+		callback(null, filtered);
+	});
+}
