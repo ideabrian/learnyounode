eaf64cb57f6cc5f723120e71bdf770fffa43c160
Brian Ball <ideabrian@gmail.com>
2014-12-17 23:32:55 -0800

    http

--- myprogram.js
+++ myprogram.js
@@ -1,3 +1,7 @@
-// Learn You Node
+var http = require('http');
 
-// See each of the branches for the code for each step.
+http.get(process.argv[2], function(response) {
+	response.setEncoding('utf8');
+	response.on("data", console.log);
+	response.on("error", console.error);
+})
