be6f2c0c36eea6a039c7db1e4687001a478fb5ae
Brian Ball <ideabrian@gmail.com>
2014-12-18 08:55:15 -0800

    http file server

--- myprogram.js
+++ myprogram.js
@@ -1,14 +1,24 @@
-var net = require('net');
+// var net = require('net');
 
-function pad(n) { return n < 10 ? '0' + n : n }
+// function pad(n) { return n < 10 ? '0' + n : n }
 
-var server = net.createServer(function(socket) {
-	d = new Date();
-	s = d.getFullYear() + '-'
-	  + pad(d.getMonth() + 1) + '-'
-	  + pad(d.getDate()) + " "
-	  + pad(d.getHours()) + ':'
-	  + pad(d.getMinutes()) + '\n';
-	socket.end(s);
+// var server = net.createServer(function(socket) {
+// 	d = new Date();
+// 	s = d.getFullYear() + '-'
+// 	  + pad(d.getMonth() + 1) + '-'
+// 	  + pad(d.getDate()) + " "
+// 	  + pad(d.getHours()) + ':'
+// 	  + pad(d.getMinutes()) + '\n';
+// 	socket.end(s);
+// });
+// server.listen(process.argv[2]);
+
+var http = require('http');
+var fs = require('fs');
+
+var fileName = process.argv[3];
+
+server = http.createServer(function(req, res) {
+	fs.createReadStream(fileName).pipe(res);
 });
 server.listen(process.argv[2]);
